9f7419a9924547c6cd40eb5c558791817e26a6ee
Jason Mandozzi <jason.mandozzi@gmail.com>
2021-01-23 17:42:49 -0500

    Initial Commit

---
+++ CryptoBot/CryptoBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBotApp.swift
+//  CryptoBot
+//
+//  Created by Jason Mandozzi on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
